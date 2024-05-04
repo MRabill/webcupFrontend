c69e6992fff5f20c7dfe1fc26995c7b6cdbb2abb
rabill.madarboccus <rabill.madarboccus@rogerscapital.mu>
2024-05-05 00:50:42 +0400

    Add training component

---
+++ src/Components/TrainingComponent.jsx
@@ -0,0 +1,202 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { twMerge } from "tailwind-merge";
+import { FiArrowRight, FiMail, FiMapPin } from "react-icons/fi";
+import { SiGithub, SiTiktok, SiTwitter, SiYoutube } from "react-icons/si";
+
+const TrainingComponent = () => {
+  return (
+    <div className="min-h-screen  px-4 py-12 text-zinc-50">
+      {/* <Logo /> */}
+      <motion.div
+        initial="initial"
+        animate="animate"
+        transition={{
+          staggerChildren: 0.05,
+        }}
+        className="mx-auto grid max-w-4xl grid-flow-dense grid-cols-12 gap-4"
+      >
+        {/* <HeaderBlock />
+         */}
+        <AboutBlock />
+        <LocationBlock />
+        <EmailListBlock />
+        <SocialsBlock />
+      </motion.div>
+      <Footer />
+    </div>
+  );
+};
+
+export default TrainingComponent;
+
+const Block = ({ className, ...rest }) => {
+  return (
+    <motion.div
+      variants={{
+        initial: {
+          scale: 0.5,
+          y: 50,
+          opacity: 0,
+        },
+        animate: {
+          scale: 1,
+          y: 0,
+          opacity: 1,
+        },
+      }}
+      transition={{
+        type: "spring",
+        mass: 3,
+        stiffness: 400,
+        damping: 50,
+      }}
+      className={twMerge(
+        "col-span-4 rounded-lg border border-zinc-700 bg-zinc-800 p-6",
+        className
+      )}
+      {...rest}
+    />
+  );
+};
+
+const SocialsBlock = () => (
+  <>
+    <Block
+      whileHover={{
+        rotate: "2.5deg",
+        scale: 1.1,
+      }}
+      className="col-span-6 bg-red-500 md:col-span-3"
+    >
+      <a
+        href="#"
+        className="grid h-full place-content-center text-3xl text-white"
+      >
+        <SiYoutube />
+      </a>
+    </Block>
+    <Block
+      whileHover={{
+        rotate: "-2.5deg",
+        scale: 1.1,
+      }}
+      className="col-span-6 bg-green-600 md:col-span-3"
+    >
+      <a
+        href="#"
+        className="grid h-full place-content-center text-3xl text-white"
+      >
+        <SiGithub />
+      </a>
+    </Block>
+    <Block
+      whileHover={{
+        rotate: "-2.5deg",
+        scale: 1.1,
+      }}
+      className="col-span-6 bg-zinc-50 md:col-span-3"
+    >
+      <a
+        href="#"
+        className="grid h-full place-content-center text-3xl text-black"
+      >
+        <SiTiktok />
+      </a>
+    </Block>
+    <Block
+      whileHover={{
+        rotate: "2.5deg",
+        scale: 1.1,
+      }}
+      className="col-span-6 bg-blue-500 md:col-span-3"
+    >
+      <a
+        href="#"
+        className="grid h-full place-content-center text-3xl text-white"
+      >
+        <SiTwitter />
+      </a>
+    </Block>
+  </>
+);
+
+const AboutBlock = () => (
+  <Block className="col-span-12 text-3xl leading-snug">
+    <p
+      style={{
+        fontFamily: "Orbitron",
+      }}
+    >
+      My passion is training superheroes to reach their full potential.
+      <br />
+      <span
+        className="text-gold-400"
+        style={{
+          fontFamily: "Orbitron",
+          fontSize: "1.0rem",
+        }}
+      >
+        I specialize in honing superpowers, combat techniques, and tactical
+        strategies. I have trained countless superheroes in mastering their
+        abilities to defend the world against evil forces.
+      </span>
+    </p>
+  </Block>
+);
+
+const LocationBlock = () => (
+  <Block className="col-span-12 flex flex-col items-center gap-4 md:col-span-3">
+    <FiMapPin className="text-3xl" />
+    <p className="text-center text-lg text-zinc-400">Cyberspace</p>
+  </Block>
+);
+
+const EmailListBlock = () => (
+  <Block className="col-span-12 md:col-span-9">
+    <p className="mb-3 text-lg">Join my mailing list</p>
+    <form
+      onSubmit={(e) => e.preventDefault()}
+      className="flex items-center gap-2"
+    >
+      <input
+        type="email"
+        placeholder="Enter your email"
+        className="w-full rounded border border-zinc-700 bg-zinc-800 px-3 py-1.5 transition-colors focus:border-red-300 focus:outline-0"
+      />
+      <button
+        type="submit"
+        className="flex items-center gap-2 whitespace-nowrap rounded bg-zinc-50 px-3 py-2 text-sm font-medium text-zinc-900 transition-colors hover:bg-zinc-300"
+      >
+        <FiMail /> Join the list
+      </button>
+    </form>
+  </Block>
+);
+
+const Logo = () => {
+  // Temp logo from https://logoipsum.com/
+  return (
+    <svg
+      width="40"
+      height="auto"
+      viewBox="0 0 50 39"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="mx-auto mb-12 fill-zinc-50"
+    >
+      <path
+        d="M16.4992 2H37.5808L22.0816 24.9729H1L16.4992 2Z"
+        stopColor="#000000"
+      ></path>
+      <path
+        d="M17.4224 27.102L11.4192 36H33.5008L49 13.0271H32.7024L23.2064 27.102H17.4224Z"
+        stopColor="#000000"
+      ></path>
+    </svg>
+  );
+};
+
+const Footer = () => {
+  return <footer className="mt-12"></footer>;
+};
