76eb0f8d0e34e52cf3b74c9fc14366e40fbee16e
rabill.madarboccus <rabill.madarboccus@rogerscapital.mu>
2024-05-05 06:26:54 +0400

    add ACL

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -5,24 +5,59 @@ import { motion } from "framer-motion";
 import lobby from "../Assets/lobby.jpg";
 import StarsCanvas from "../Components/canvas/Stars";
 import UserProfile from "./userprofile";
+import { useUser } from "../Context/UserContext";
+import background from "../Assets/landingPage.jpg";
+import { useNavigate } from "../../utils";
 
 const Profile = () => {
+  const { user, signing, logout } = useUser();
+  const navigate = useNavigate();
   return (
     <div>
-      <div
-        style={{
-          width: "100vw",
-          height: "100vh",
-          background: "#131225",
-          position: "absolute",
-          zIndex: "-1000",
-        }}
-      ></div>
-      <div></div>
-      <div className="relativve z-0">
-        <UserProfile />
-        <StarsCanvas />
-      </div>
+      {user?.username !== "" ? (
+        <>
+          <div
+            style={{
+              width: "100vw",
+              height: "100vh",
+              background: "#131225",
+              position: "absolute",
+              zIndex: "-1000",
+            }}
+          ></div>
+          <div></div>
+          <div className="relativve z-0">
+            <UserProfile />
+            <StarsCanvas />
+          </div>
+        </>
+      ) : (
+        <>
+          <img
+            src={background}
+            style={{
+              position: "absolute",
+              height: "100vh",
+              width: "100%",
+              zIndex: "-1000",
+            }}
+          />
+          <div
+            style={{
+              height: "100vh",
+              width: "100vw",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <button className="kave-btn" onClick={() => navigate("/")}>
+              <span className="kave-line"></span>
+              "Sign in to continue"
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- src/Pages/Training.jsx
+++ src/Pages/Training.jsx
@@ -7,9 +7,14 @@ import Avatars4Canvas from "../Components/canvas/avatar4";
 import Avatars5Canvas from "../Components/canvas/avatar5";
 import StarsCanvas from "../Components/canvas/Stars";
 import TrainingComponent from "../Components/TrainingComponent";
+import background from "../Assets/landingPage.jpg";
+import { useUser } from "../Context/UserContext";
+import { useNavigate } from "../../utils";
 
 const Training = () => {
   const [selectedAvatar, setSelectedAvatar] = useState("avatar5");
+  const { user, signing, logout } = useUser();
+  const navigate = useNavigate();
 
   const handleAvatarClick = (avatarType) => {
     setSelectedAvatar(avatarType);
@@ -22,39 +27,69 @@ const Training = () => {
   };
 
   return (
-    <>
-      <div className="background" />
+    <div>
+      {user?.username !== "" ? (
+        <>
+          <div className="background" />
 
-      {/* Stars animation */}
-      <div className="relativve z-1000">
-        <StarsCanvas />
-      </div>
+          {/* Stars animation */}
+          <div className="relativve z-1000">
+            <StarsCanvas />
+          </div>
 
-      {/* Main content */}
-      <div className="training-container">
-        <Col lg={8} md={24} xs={24}>
-          <div className="left-section">
-            <div className="flex justify-between items-center h-screen">
-              {/* Render the selected avatar */}
-              {selectedAvatar === "avatar4" && <Avatars4Canvas />}
-              {selectedAvatar === "avatar5" && <Avatars5Canvas />}
-              {/* Add more conditions for other avatars if needed */}
-            </div>
+          {/* Main content */}
+          <div className="training-container">
+            <Col lg={8} md={24} xs={24}>
+              <div className="left-section">
+                <div className="flex justify-between items-center h-screen">
+                  {/* Render the selected avatar */}
+                  {selectedAvatar === "avatar4" && <Avatars4Canvas />}
+                  {selectedAvatar === "avatar5" && <Avatars5Canvas />}
+                  {/* Add more conditions for other avatars if needed */}
+                </div>
+              </div>
+            </Col>
+            <Col lg={16} md={24} xs={24}>
+              <div className="right-section">
+                <Row>
+                  {/* Render the TrainingComponent passing selectedAvatar */}
+                  <TrainingComponent
+                    onChangeAvatar={changeAvatar}
+                    selectedAvatar={selectedAvatar}
+                  />
+                </Row>
+              </div>
+            </Col>
           </div>
-        </Col>
-        <Col lg={16} md={24} xs={24}>
-          <div className="right-section">
-            <Row>
-              {/* Render the TrainingComponent passing selectedAvatar */}
-              <TrainingComponent
-                onChangeAvatar={changeAvatar}
-                selectedAvatar={selectedAvatar}
-              />
-            </Row>
+        </>
+      ) : (
+        <>
+          <img
+            src={background}
+            style={{
+              position: "absolute",
+              height: "100vh",
+              width: "100%",
+              zIndex: "-1000",
+            }}
+          />
+          <div
+            style={{
+              height: "100vh",
+              width: "100vw",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <button className="kave-btn" onClick={() => navigate("/")}>
+              <span className="kave-line"></span>
+              "Sign in to continue"
+            </button>
           </div>
-        </Col>
-      </div>
-    </>
+        </>
+      )}
+    </div>
   );
 };
 
